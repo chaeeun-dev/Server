5515179eb2fb9734efdffb1361c9d4f9cbeec439
Im Chaeeun <dev.chaeng@gmail.com>
2024-09-30 21:49:21 +0900

    쓰레드 생성 강의 수강

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,20 +1,37 @@
-﻿// Server.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+using namespace std;
+#include <thread>
+#include <vector>
 
-#include <iostream>
+// 메모리
+// Code - 코드 영역은 수정하지 않기 때문에 문제 없음
+// Stack - 쓰레드마다 고유한 스택 영역 사용하기에 문제 없음
+//
+// Heap O
+// Data O
+// 이 두 영역은 공유해서 사용할 수 있기 때문에 복잡함...
+
+void HelloThread(int i)
+{
+	while (true)
+	{
+		cout << "Hello Thread" << i << endl;
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	vector<thread> threads;		// 여러 개의 쓰레드를 만들 때 vector 사용해도 됨
+
+	for (int i = 0; i < 12; ++i)
+	{
+		threads.push_back(thread(HelloThread, i));
+	}
+
+	// 자식 쓰레드가 메인 쓰레드가 종료되어도 끝나지 않으면 오류 발생 -> join() 
+	for (thread& t :threads)
+	{
+		int a = t.hardware_concurrency();	// CPU 코어 개수 알 수 있음 - 12개, 12개 이상으로 늘려도 빨라지지 않음
+		t.join();
+	}
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
